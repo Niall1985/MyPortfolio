492be95778fad312674c70f37905d25b9fe99ec1
Niall1985 <dcunhaniall@gmail.com>
2025-09-21 12:30:58 +0530

    updated

--- myportfolio/src/App.tsx
+++ myportfolio/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Github, Linkedin, Twitter, Mail, ExternalLink, Code, Database, Wrench, ChevronDown, Star } from 'lucide-react';
 
 const App = () => {
